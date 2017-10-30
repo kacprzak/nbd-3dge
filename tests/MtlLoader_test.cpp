2aa855dc812aa6527e9354890c0b959196f3000d
Marcin Kacprzak <martin.kacprzak@gmail.com>
2017-10-30 19:37:30 +0100

    Enabled testing

---
+++ tests/MtlLoader_test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE MtlLoaderTest
+#include <boost/test/unit_test.hpp>
+
+#include <MtlLoader.h>
+
+BOOST_AUTO_TEST_CASE(MtlLoader_test)
+{
+    MtlLoader ldr;
+}
