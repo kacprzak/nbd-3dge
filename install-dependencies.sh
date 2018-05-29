79a87fee316a761551ca2182081e946087d4a2ae
Marcin Kacprzak <martin.kacprzak@gmail.com>
2018-05-29 19:41:40 +0200

    Added definitions required by glm

---
+++ install-dependencies.sh
@@ -0,0 +1,1 @@
+apt-get install -y libglm-dev libglew-dev libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev libpthread-stubs0-dev libbullet-dev libboost-dev libboost-log-dev libboost-program-options-dev libboost-test-dev libboost-regex-dev
