836cf9a6bb1eac0d283bcb9b830c1bcc90ff0dd8
Marcin Kacprzak <martin.kacprzak@gmail.com>
2018-07-21 20:10:05 +0200

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 apt-get install -y libegl1-mesa-dev libxcursor-dev libxinerama-dev libxi-dev libxrandr-dev libxss-dev
-apt-get install libc++-dev
 apt-get install -y unzip
 
 mkdir external
