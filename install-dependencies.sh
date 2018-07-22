0e24627b279c0b843bd5e77ce016404d33a60e3a
Marcin Kacprzak <martin.kacprzak@gmail.com>
2018-07-22 11:44:05 +0200

    Add pulse to required libs

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt-get install -y libegl1-mesa-dev libxcursor-dev libxinerama-dev libxi-dev libxrandr-dev libxss-dev libasound2-dev libjack-dev
+apt-get install -y libegl1-mesa-dev libxcursor-dev libxinerama-dev libxi-dev libxrandr-dev libxss-dev libasound2-dev libjack-dev libpulse-dev
 apt-get install -y unzip
 
 mkdir external
