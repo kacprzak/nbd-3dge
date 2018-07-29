8ccc36478b304fd2986abf59a1c42c149976ccbb
Marcin Kacprzak <martin.kacprzak@gmail.com>
2018-07-29 11:51:12 +0200

    Moved deps to travis.yml

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-apt-get install -y libegl1-mesa-dev libxcursor-dev libxinerama-dev libxi-dev libxrandr-dev libxss-dev libasound2-dev libjack-dev libpulse-dev libaudio-dev
-apt-get install -y unzip
-
 mkdir external
 cd external
 
