f6d2934855b7ebb6c01f5abb5b10a31403f90107
Marcin Kacprzak <martin.kacprzak@gmail.com>
2018-07-29 12:12:37 +0200

    install-dependencies.py

---
+++ install-dependencies.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import urllib.request
+import zipfile
+
+external_dir = "external"
+os.makedirs(external_dir, exist_ok=True)
+os.chdir(external_dir)
+
+zip_file = 'master.zip'
+urllib.request.urlretrieve('https://github.com/g-truc/gli/archive/master.zip', zip_file)
+
+zip_ref = zipfile.ZipFile(zip_file, 'r')
+zip_ref.extractall('.')
+zip_ref.close()
+
+os.rename('gli-master', 'gli')
+
+os.chdir('..')
