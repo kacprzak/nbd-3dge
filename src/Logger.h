e3500a383486080691943bebbacf020648d97b90
Marcin Kacprzak <martin.kacprzak@gmail.com>
2018-12-13 15:38:50 +0100

    Win output logging (2)

--- src/Logger.h
+++ src/Logger.h
@@ -2,7 +2,11 @@
 #define LOGGER_H
 
 #include <spdlog/spdlog.h>
+#ifdef _MSC_VER
+#include <spdlog/sinks/msvc_sink.h>
+#else
 #include <spdlog/sinks/stdout_sinks.h>
+#endif
 
 #define LOG_TRACE(...) spdlog::get("console")->log(spdlog::level::trace, __VA_ARGS__)
 #define LOG_DEBUG(...) spdlog::get("console")->log(spdlog::level::debug, __VA_ARGS__)
